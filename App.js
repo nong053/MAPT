5d1b97b5ce13cf19b6a243216b3d8b3cbfd349f7
KOSIT AROMSAVA <kosit.arom@gmail.com>
2023-04-23 17:33:20 +0700

    commit

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
 import * as React from 'react';
 import { useRef } from 'react';
 import 'expo-dev-client';
-
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
-
 const adUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-9478268987509661/9602170968';
-
-
-
 import Constants from 'expo-constants';
 //import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { FontAwesome } from '@expo/vector-icons';
+
+
 
 
 
@@ -30,6 +27,9 @@ function wait(timeout) {
 
 export default function App() {
 
+  
+   const UUID =  Math.floor(Math.random() * 10000000000000000);
+
   const [refreshing, setRefreshing] = React.useState(false);
   const webViewRef = useRef()
   
@@ -37,21 +37,32 @@ export default function App() {
     setRefreshing(true);
     webViewRef.current.reload();
     wait(2000).then(() => setRefreshing(false));
+
   }, [refreshing]);
 
+  
+
   return (
+
+  
     <View style={{flex: 1,backgroundColor:'#191970'}}>
   
       <SafeAreaView>
           <View  style={styles.buttonRefreshArea} >
               <FontAwesome style={styles.buttonRefresh} name={'refresh'} onPress={onRefresh} /> 
+              {/* <Text>{uuidv4()}</Text> */}
           </View>
+          <View>
+         
+          </View>
+          
       </SafeAreaView>
          
         <WebView
+
           ref = {webViewRef}
           automaticallyAdjustContentInsets={false}
-          source={{ uri: 'http://dashboardweb.com/swot' }} 
+          source={{ uri: 'http://dashboardweb.com/swot/index.php?uuid='+UUID+''}} 
           allowsFullscreenVideo={true}
           javaScriptEnabled={true}
           domStorageEnabled={true}
@@ -62,7 +73,7 @@ export default function App() {
     
     <SafeAreaView style={styles.container}>
       <BannerAd 
-         
+   
             unitId={adUnitId}
             size={BannerAdSize.LARGE_BANNER}
             requestOptions={{
@@ -79,12 +90,15 @@ const styles = StyleSheet.create({
   container: {
    // position : 'relative',
    marginTop:0,
-   borderColor:'#000039',
-   borderTopWidth:2,
+   //borderColor:'#000039',
+  // borderTopWidth:2,
    padding:5,
    //borderBottomWidth:1,
   justifyContent: 'center',
+  backgroundColor:'white'
+
  // height:80
+
   
   
  
@@ -103,11 +117,13 @@ const styles = StyleSheet.create({
   buttonRefresh:{
    
     justifyContent:'center',
-    fontSize:15,
+    fontSize:20,
     color:'#ffffff',
     textAlign:'center',
     
     
-  },
+  }
  
 });
+//dev: eas build --profile development --platform android =apk then run: expo startt -dev-client
+//production: eas build --platform android=abb then deploy to google play console for productionr
